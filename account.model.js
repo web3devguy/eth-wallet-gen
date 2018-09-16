e3db24c14189c6be17ffea4ea9076033d803ea4d
insideoutzombie <david@web3devs.com>
2018-09-16 14:56:04 -0500

    add code content to account.model schema file

--- account.model.js
+++ account.model.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// this is our schema file for the mongodb
+
 let AccountSchema = new Schema({
   ETH: {type: String, required: false, max: 64},
 });
